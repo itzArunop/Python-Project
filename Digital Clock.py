d195f46198e5e2705a64bebf6ae56f1f68388b2f
Arun Katyal <arunkatyal03@gmail.com>
2025-10-05 17:52:21 +0530

    Add files via upload

---
+++ Digital Clock.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from time import strftime
+
+#Create the main window
+root = tk.Tk()
+root.title("Digital Clock")
+
+#Function to update time
+def time():
+    string = strftime('%I:%M:%S %p \n %D') #Format: Hour:Minute:Second AM/PM
+    label.config(text=string)
+    label.after(1000,time) #Update every 1 second
+
+#Styling the clock
+label = tk.Label(root,font=('Algerian',50,'bold'),background='blue',foreground='black')
+label.pack(anchor='center')
+
+time() #Run the clock
+
+root.mainloop()
